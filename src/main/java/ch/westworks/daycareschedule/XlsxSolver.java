51ceef1be74abac32711e2c32ea5acf14607ac0d
Christoph Leuzinger <leuzi@trash.net>
2020-11-23 22:46:25 +0100

    Add support for XSLX input/output

---
+++ src/main/java/ch/westworks/daycareschedule/XlsxSolver.java
@@ -0,0 +1,38 @@
+
+package ch.westworks.daycareschedule;
+
+import ch.westworks.daycareschedule.xlsx.XlsxInputReader;
+import ch.westworks.daycareschedule.xlsx.XlsxOutputWriter;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+
+import static java.awt.Desktop.getDesktop;
+
+public class XlsxSolver {
+
+    public static final String XLSX = "xlsx";
+    public static final String XLSX_SUFFIX = "." + XLSX;
+
+    public static void main(String[] args) {
+        final JFileChooser fileChooser = new JFileChooser();
+        fileChooser.addChoosableFileFilter(new FileNameExtensionFilter("Excel", XLSX));
+        fileChooser.setAcceptAllFileFilterUsed(false);
+        fileChooser.showOpenDialog(null);
+
+        try {
+            final File file = fileChooser.getSelectedFile();
+            if (file != null) {
+                final Input input = new XlsxInputReader().readInput(file);
+                final Solution solution = new Solver(input).solve();
+                final File output = File.createTempFile(file.getName().replace(XLSX_SUFFIX, "_Solution_"), XLSX_SUFFIX, file.getParentFile());
+                new XlsxOutputWriter().writeOutput(output, input, solution);
+                getDesktop().open(output);
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getClass().getSimpleName() + ": " + e.getLocalizedMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+            e.printStackTrace();
+        }
+    }
+}
